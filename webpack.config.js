e388538d2fda616fcea1811b70e1dc9798ad019c
Joao Lopes <lopis@users.noreply.github.com>
2021-04-09 09:28:20 +0200

    Initial boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+ template: "./src/index.html",
+ filename: "./index.html"
+});
+
+var config = {
+  entry: ["./src/index.tsx"],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    htmlPlugin
+  ]
+};
+
+module.exports = config;

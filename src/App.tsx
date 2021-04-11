a2d66e06ed3172e9b8e3503284b79500cf8ff2bc
kantimam <kantemir.imam@gmail.com>
2021-04-12 01:05:03 +0200

    turned text and button in header into links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import Home from './pages/Home';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
-import { JobPageRoute } from './pages/JobPage';
+import React from "react";
+import Home from "./pages/Home";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { JobPageRoute } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
   return (
@@ -12,6 +13,9 @@ const App = () => {
       <Switch>
         <Route path="/job/:jobId">
           <JobPageRoute />
+        </Route>
+        <Route path="/add">
+          <AddJobPage />
         </Route>
         <Route>
           <Home />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 // import MenuIcon from '@material-ui/icons/Menu';
-import PublishIcon from '@material-ui/icons/Publish';
+import PublishIcon from "@material-ui/icons/Publish";
+import { Link } from "react-router-dom";
+import { Box } from "@material-ui/core";
 
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
       flexGrow: 1,
     },
-    title: {
-      flexGrow: 1,
+    titleContainer: { flexGrow: 1 },
+    unstyledLink: {
+      textDecoration: "inherit",
     },
   })
 );
@@ -25,16 +28,26 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            ReDi Community Job Board
-          </Typography>
-          <Button
-            variant="contained"
-            color="default"
-            startIcon={<PublishIcon />}
-          >
-            Publish Job Listing
-          </Button>
+          <Box className={classes.titleContainer}>
+            <Typography
+              to="/"
+              component={Link}
+              variant="h6"
+              className={classes.unstyledLink}
+              color="inherit"
+            >
+              ReDi Community Job Board
+            </Typography>
+          </Box>
+          <Link to="/add" className={classes.unstyledLink}>
+            <Button
+              variant="contained"
+              color="default"
+              startIcon={<PublishIcon />}
+            >
+              Publish Job Listing
+            </Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </div>

257ddccc6567121bc83e53eb8ef8830c0783932b
kantimam <kantemir.imam@gmail.com>
2021-04-12 18:10:33 +0200

    lint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
-import MenuIcon from "@material-ui/icons/Menu";
-import PublishIcon from "@material-ui/icons/Publish";
-import { Link } from "react-router-dom";
-import Box from "@material-ui/core/Box";
-import Hidden from "@material-ui/core/Hidden";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import IconButton from "@material-ui/core/IconButton";
+import React, { useState } from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import PublishIcon from '@material-ui/icons/Publish';
+import { Link } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Hidden from '@material-ui/core/Hidden';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import IconButton from '@material-ui/core/IconButton';
 
-import rediLogo from "../static/redi-logo.png";
+import rediLogo from '../static/redi-logo.png';
 
 const useStyles = makeStyles(() =>
   createStyles({
     titleContainer: { flexGrow: 1 },
     unstyledLink: {
-      textDecoration: "inherit",
+      textDecoration: 'inherit',
     },
     logo: {
-      height: "48px",
+      height: '48px',
     },
   })
 );

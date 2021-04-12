f565d52af185734599725b9559dcd4ae20e703b3
kantimam <kantemir.imam@gmail.com>
2021-04-12 17:19:25 +0200

    mobile appbar added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-// import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from "@material-ui/icons/Menu";
 import PublishIcon from "@material-ui/icons/Publish";
 import { Link } from "react-router-dom";
-import { Box } from "@material-ui/core";
+import { Box, Hidden, IconButton, Drawer, List } from "@material-ui/core";
 // @ts-ignore
 import rediLogo from "../static/redi-logo.png";
+import ListItem from "@material-ui/core/ListItem";
 
 const useStyles = makeStyles(() =>
   createStyles({
-    root: {
-      flexGrow: 1,
-    },
     titleContainer: { flexGrow: 1 },
     unstyledLink: {
       textDecoration: "inherit",
@@ -28,10 +26,11 @@ const useStyles = makeStyles(() =>
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
+    <>
+      <AppBar position="fixed">
         <Toolbar>
           <Box className={classes.titleContainer}>
             {/* <Typography
@@ -51,17 +50,44 @@ export default function ButtonAppBar() {
               />
             </Link>
           </Box>
-          <Link to="/add" className={classes.unstyledLink}>
-            <Button
-              variant="contained"
-              color="default"
-              startIcon={<PublishIcon />}
+          <Hidden smDown>
+            <Link to="/add" className={classes.unstyledLink}>
+              <Button
+                variant="contained"
+                color="default"
+                startIcon={<PublishIcon />}
+              >
+                Publish Job Listing
+              </Button>
+            </Link>
+          </Hidden>
+          <Hidden mdUp>
+            <IconButton onClick={() => setDrawerOpen(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              open={drawerOpen}
+              anchor="right"
+              onClose={() => setDrawerOpen(false)}
             >
-              Publish Job Listing
-            </Button>
-          </Link>
+              <List>
+                <ListItem>
+                  <Link to="/add" className={classes.unstyledLink}>
+                    <Button
+                      variant="contained"
+                      color="default"
+                      startIcon={<PublishIcon />}
+                    >
+                      Publish Job Listing
+                    </Button>
+                  </Link>
+                </ListItem>
+              </List>
+            </Drawer>
+          </Hidden>
         </Toolbar>
       </AppBar>
-    </div>
+      <Toolbar />
+    </>
   );
 }

f95a1eaa0335568bb29b37d5ece81382218bbef7
Joao Lopes <lopis@users.noreply.github.com>
2021-04-26 08:44:48 +0200

    Add simple unit test to homepage

---
+++ src/__tests__/e2e.test.jsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import jobsData from '../../mocks/jobs.json';
+
+const mockFetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(jobsData),
+    ok: true,
+  })
+);
+global.fetch = mockFetch;
+
+describe('Test Suite', () => {
+  it('click on a button', async () => {
+    const { findAllByTestId } = render(<App />);
+    const jobs = await findAllByTestId('job');
+    expect(mockFetch).toHaveBeenCalled();
+    expect(jobs.length).toEqual(jobsData.jobs.length);
+
+  });
+});

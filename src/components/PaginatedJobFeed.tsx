8702ef4f6f4a20677b0f8965599f6fa4d52f50b2
Nyannazz <kantemir.imam@gmail.com>
2021-05-11 01:37:49 +0200

    added next button

--- src/components/PaginatedJobFeed.tsx
+++ src/components/PaginatedJobFeed.tsx
@@ -3,8 +3,9 @@ import { IJob } from '../types';
 import FetchWrapper from './FetchWrapper';
 import JobListing from './JobListing';
 import {v4 as uuid} from 'uuid';
-import {useLocation} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import { JOBS } from '../api/useFetch';
+import Button from '@material-ui/core/Button';
 
 interface Props {
     pageSize?: number
@@ -13,20 +14,30 @@ interface Props {
 function useQuery(): URLSearchParams {
   return new URLSearchParams(useLocation().search);
 }
+function usePageQuery(): number {
+  const pageQuery=useQuery().get('page');
+  const pageNumber=pageQuery ? parseInt(pageQuery) : 1;
+  return pageNumber>0? pageNumber : 1;
+}
+
+function setPageInQueryString(query:string, page: number): string {
+  const queryObject=new URLSearchParams(query);
+  queryObject.set('page', String(page));
+  return queryObject.toString();
+}
 
 const PaginatedJobFeed = ({pageSize=20}: Props) => {
   const [data, setData]=useState([]);
-  const [pagination, setPagination]=useState();
+  //const [pagination, setPagination]=useState();
   const [isLoading, setIsLoading]=useState<boolean>(false);
   const [error, setError]=useState(null);
-  const pageQuery=useQuery().get('page');
+  const page: number=usePageQuery();
     
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const pageNumber=pageQuery ? parseInt(pageQuery) : 1;
-        const page: number=pageNumber>0? pageNumber : 1;
+        
         const skip=(page - 1) * pageSize;
         const response = await fetch(`${JOBS}?skip=${skip}&limit=${pageSize}`);
         const result = await response.json();
@@ -44,13 +55,20 @@ const PaginatedJobFeed = ({pageSize=20}: Props) => {
       }
     };
     fetchData();
-  }, [pageQuery, pageSize]);
+  }, [page, pageSize]);
+
 
 
   return <FetchWrapper isLoading={isLoading} hasError={!!error} data={data}>
-    {({data}:{data: IJob[]})=><>
+    {({data}:{data: IJob[]})=><div>
       {data.map((job: IJob)=><JobListing job={job} key={uuid()}/>)}
-    </>
+      <Link to={location=>({...location, search: setPageInQueryString(location.search, page + 1)})}>
+        <Button color="primary" variant="contained" onClick={()=>console.log('asd')}>
+            Next
+        </Button>
+      </Link>
+      
+    </div>
     }
   </FetchWrapper>;
 };

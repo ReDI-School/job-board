0fc4fb6214802dde9ea0431a6149b8163b122421
kantimam <kantemir.imam@gmail.com>
2021-06-02 15:37:34 +0200

    removed footer box shadow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
 interface IProps {
-  containerMaxWidth?:  false | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  containerMaxWidth?: false | 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -17,46 +17,53 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       flexGrow: 1,
     },
-   
+
     link: {
       color: '#333',
       fontFamily: 'Arial',
       fontSize: '1rem',
-      textDecoration: 'none'
+      textDecoration: 'none',
     },
     icon: {
       color: '#333',
-      marginRight: '0.3em'
-    }
-  }),
+      marginRight: '0.3em',
+    },
+  })
 );
 
-const Footer = ({containerMaxWidth='lg'}: IProps) => {
+const Footer = ({ containerMaxWidth = 'lg' }: IProps) => {
   const classes = useStyles();
   return (
-    <Box component="footer" mt={8} py={2} boxShadow={4} >
+    <Box component="footer" mt={8} py={2}>
       <Container maxWidth={containerMaxWidth}>
-        <Grid container justify='space-between'>
-          <Grid item className={classes.link} >
+        <Grid container justify="space-between">
+          <Grid item className={classes.link}>
             <Hidden mdDown>
-              <Grid container justify='space-around' spacing={2}>
-                <Grid item  /*component={Link} to="/"*/ >
-                Home</Grid>
-                <Grid item  /*component={Link} to="/About"*/ >
-                Redi School</Grid>
-                <Grid item  /*component={Link} to="/Redi"*/ >
-                Copyright</Grid>
+              <Grid container justify="space-around" spacing={2}>
+                <Grid item /*component={Link} to="/"*/>Home</Grid>
+                <Grid item /*component={Link} to="/About"*/>Redi School</Grid>
+                <Grid item /*component={Link} to="/Redi"*/>Copyright</Grid>
               </Grid>
             </Hidden>
           </Grid>
-          <Grid item className={classes.link} >
+          <Grid item className={classes.link}>
             <Grid container>
-              <Grid item component={'a'} href="https://www.facebook.com/search/top?q=redi%20school%20of%20digital%20integration"
-                rel="noopener noreferrer" target="_blank">
+              <Grid
+                item
+                component={'a'}
+                href="https://www.facebook.com/search/top?q=redi%20school%20of%20digital%20integration"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
                 <Facebook className={classes.icon} />
               </Grid>
-              <Grid item component={'a'} href="https://twitter.com/ReDISchool"
-                rel="noopener noreferrer" target="_blank">
+              <Grid
+                item
+                component={'a'}
+                href="https://twitter.com/ReDISchool"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
                 <Tweeter className={classes.icon} />
               </Grid>
             </Grid>
